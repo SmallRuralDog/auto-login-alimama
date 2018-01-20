cfc936b3b88842e3ed3b8e63c50b547f9df1d582
小土狗 <296404875@qq.com>
2018-01-20 10:34:50 +0800

    Update alimama.py

--- alimama.py
+++ alimama.py
@@ -1,4 +1,5 @@
 # coding:utf-8
+import datetime
 import json
 import time
 
@@ -9,8 +10,6 @@ from selenium import webdriver
 class Spider(object):
     def __init__(self):
         self.web = webdriver.Chrome()
-        self.__username = '淘宝账号'
-        self.__password = '淘宝密码'
         self.headers = {
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
             'accept-encoding': 'gzip, deflate, br',
@@ -21,18 +20,15 @@ class Spider(object):
         }
         self.req = requests.Session()
         self.cookies = {}
-    #登录
+
     def login(self):
         self.web.get(
             'https://login.taobao.com/member/login.jhtml?style=mini&newMini2=true&css_style=alimama&from=alimama&redirectURL=http%3A%2F%2Fwww.alimama.com&full_redirect=true&disableQuickLogin=true')
-        self.web.find_element_by_class_name('login-switch').click()
 
-        self.web.find_element_by_id('TPL_username_1').send_keys(self.__username)
-        self.web.find_element_by_id('TPL_password_1').send_keys(self.__password)
-        time.sleep(2)
-        self.web.find_element_by_id('J_SubmitStatic').click()
-        # 等待5秒
-        time.sleep(5)
+        i = input('请确认是否已登录？[y/n]:')
+        if (i != 'y'):
+            return
+
         self.web.get('http://pub.alimama.com/myunion.htm')
         cookie = ''
         for elem in self.web.get_cookies():
@@ -45,38 +41,20 @@ class Spider(object):
 
     # 获取淘宝客订单列表
     def get_taoke_order_list(self):
-        url = 'http://pub.alimama.com/report/getTbkPaymentDetails.json?startTime=2017-05-28&endTime=2017-08-03&payStatus=&queryType=1&toPage=1&perPageSize=50&total=&t=1501747895837&pvid=&_tb_token_=7fG7CEIuRsq&_input_charset=utf-8'
+        s_day = datetime.date.today() - datetime.timedelta(days=90)
+        e_day = datetime.date.today() - datetime.timedelta(days=1)
+        url = 'https://pub.alimama.com/report/getTbkPaymentDetails.json?startTime=' + s_day.strftime(
+            "%Y-%m-%d") + '&endTime=' + e_day.strftime(
+            "%Y-%m-%d") + '&payStatus=&queryType=1&toPage=1&perPageSize=20'
+        print(url)
         web_data = self.req.get(url, headers=self.headers)
-
         data = json.loads(web_data.text)
-        print(data['data']['paymentList'])
-
-    #创建推广位
-    def add_ad(self):
-        name = input()
-        res = self.req.post('http://pub.alimama.com/common/adzone/selfAdzoneCreate.json', data={
-            'tag': '29',
-            'gcid': '8',
-            'siteid': 'xxxxxxxx',#这里改成导购位ID
-            'selectact': 'add',
-            'newadzonename': name,
-            '_tb_token_': self.token
-        }, headers=self.headers)
-
-        print(res.text)
-    #获取推广位列表
-    def get_ad_list(self):
-        res = self.req.get(
-            'http://pub.alimama.com/common/adzone/adzoneManage.json?tab=3&toPage=1&perPageSize=40&gcid=8',
-            headers=self.headers)
-        print(res.text)
+        print(data)
 
 
 if __name__ == '__main__':
     sp = Spider()
     sp.login()
-    sp.add_ad()
-    sp.get_ad_list()
-    for i in range(1000):
+    while True:
         sp.get_taoke_order_list()
         time.sleep(30)
